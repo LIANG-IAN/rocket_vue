ea5e4c09494bf79c298edf34f308b0647c8c6ba0
ASUS <ms0565119@hotmail.com>
2023-06-16 17:30:37 +0800

    npm sweetalert2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<script setup>
-
+<script>
+export default {
+  methods: {
+    showAlert() {
+      // Use sweetalert2
+      this.$swal('Hello Vue world!!!');
+    },
+  },
+};
 </script>
 
 <template>
-
+  <h1>111</h1>
+  <button @click="showAlert">Hello world</button>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './stylesheet/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -12,11 +13,16 @@ library.add(fas);
 import App from './App.vue'
 import router from './router'
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(createPinia())
 app.use(router)
+
+app.use(VueSweetalert2);
 
 app.mount('#app')
